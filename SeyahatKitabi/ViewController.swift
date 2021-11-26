6618e1ed8f9285cebfc033fbaa343da72523d351
nursah <nursahari@hotmail.com>
2021-11-26 20:42:38 +0300

    Initial Commit

---
+++ SeyahatKitabi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeyahatKitabi
+//
+//  Created by Nurşah on 26.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
